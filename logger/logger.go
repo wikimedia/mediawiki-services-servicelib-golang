5d9565c8c99719bdc4673dd7b6a96c21293b33e6
Eric Evans <eevans@wikimedia.org>
2021-05-20 17:12:54 -0500

    Consistent ordering of structs, methods, and functions
    Note: This changeset makes no normative code changes.

--- logger/logger.go
+++ logger/logger.go
@@ -36,12 +36,15 @@ const (
 	FATAL
 )
 
-// Logger formats and delivers log messages (see: NewLogger()).
-type Logger struct {
-	writer      io.Writer
-	serviceName string
-	serviceType string
-	logLevel    Level
+// LogMessage represents JSON serializable log messages.
+type LogMessage struct {
+	Timestamp string      `json:"@timestamp"`
+	Message   string      `json:"message"`
+	Client    *ecsClient  `json:"client,omitempty"`
+	Log       ecsLog      `json:"log"`
+	Network   *ecsNetwork `json:"network,omitempty"`
+	Service   ecsService  `json:"service"`
+	Trace     *ecsTrace   `json:"trace,omitempty"`
 }
 
 type ecsClient struct {
@@ -65,17 +68,6 @@ type ecsService struct {
 
 type ecsTrace struct {
 	ID string `json:"id,omitempty"`
-}
-
-// LogMessage represents JSON serializable log messages.
-type LogMessage struct {
-	Timestamp string      `json:"@timestamp"`
-	Message   string      `json:"message"`
-	Client    *ecsClient  `json:"client,omitempty"`
-	Log       ecsLog      `json:"log"`
-	Network   *ecsNetwork `json:"network,omitempty"`
-	Service   ecsService  `json:"service"`
-	Trace     *ecsTrace   `json:"trace,omitempty"`
 }
 
 // RequestScopedLogger formats and delivers a Logger and optional LogMessage attributes.
@@ -145,9 +137,80 @@ func (s *RequestScopedLogger) Trace(id string) *RequestScopedLogger {
 	return s
 }
 
+// Logger formats and delivers log messages (see: NewLogger()).
+type Logger struct {
+	writer      io.Writer
+	serviceName string
+	serviceType string
+	logLevel    Level
+}
+
+// NewLogger creates a new Logger instance using arguments for an io.Writer, service name and type, and
+// a log level.
+//
+// The serviceName and serviceType arguments correspond to service.name and service.type in the ECS
+// specification (see: https://doc.wikimedia.org/ecs/#ecs-service); Broadly speaking, service type refers
+// to the software system implementing logging, and service name to the instance of that system sending
+// log data.
+//
+// The logLevel argument must be set to one of DEBUG, INFO, WARNING, ERROR, or FATAL.  Only messages
+// logged at this level - or higher - are formatted and output.
+func NewLogger(writer io.Writer, serviceName, serviceType string, logLevel Level) (*Logger, error) {
+
+	if !validLevel(logLevel) {
+		return nil, fmt.Errorf("Unsupported log level: %d", logLevel)
+	}
+
+	return &Logger{writer, serviceName, serviceType, logLevel}, nil
+}
+
 // Request creates and returns a request-scoped Logger
 func (l *Logger) Request() *RequestScopedLogger {
 	return &RequestScopedLogger{logger: l}
+}
+
+// Debug logs messages of severity DEBUG.
+func (l *Logger) Debug(format string, v ...interface{}) {
+	l.log(DEBUG, l.basicLogMessage(DEBUG, format, v...))
+}
+
+// Info logs messages of severity INFO.
+func (l *Logger) Info(format string, v ...interface{}) {
+	l.log(INFO, l.basicLogMessage(INFO, format, v...))
+}
+
+// Warning logs messages of severity WARNING.
+func (l *Logger) Warning(format string, v ...interface{}) {
+	l.log(WARNING, l.basicLogMessage(WARNING, format, v...))
+}
+
+// Error logs messages of severity ERROR.
+func (l *Logger) Error(format string, v ...interface{}) {
+	l.log(ERROR, l.basicLogMessage(ERROR, format, v...))
+}
+
+// Fatal logs messages of severity FATAL.
+func (l *Logger) Fatal(format string, v ...interface{}) {
+	l.log(FATAL, l.basicLogMessage(FATAL, format, v...))
+}
+
+// Write logs messages of severity WARNING.  This method satisfies the io.Writer
+// interface so that Logger instances can be used as output for Golang's log module.
+func (l *Logger) Write(bytes []byte) (int, error) {
+	l.log(WARNING, l.basicLogMessage(WARNING, strings.TrimSuffix(string(bytes), "\n")))
+	return len(bytes), nil
+}
+
+// This is an (internal) utility method for creating simple LogMessage (functions).
+func (l *Logger) basicLogMessage(level Level, format string, v ...interface{}) func() LogMessage {
+	return func() LogMessage {
+		return LogMessage{
+			Message:   fmt.Sprintf(format, v...),
+			Timestamp: time.Now().Format(time.RFC3339),
+			Service:   ecsService{Name: l.serviceName, Type: l.serviceType},
+			Log:       ecsLog{Level: LevelString(level)},
+		}
+	}
 }
 
 // This is an internal implementation; The application should log messages
@@ -181,62 +244,10 @@ func (l *Logger) log(level Level, msg func() LogMessage) {
 	l.send(string(str))
 }
 
-// Fatal logs messages of severity FATAL.
-func (l *Logger) Fatal(format string, v ...interface{}) {
-	l.log(FATAL, l.basicLogMessage(FATAL, format, v...))
-}
-
-// Error logs messages of severity ERROR.
-func (l *Logger) Error(format string, v ...interface{}) {
-	l.log(ERROR, l.basicLogMessage(ERROR, format, v...))
-}
-
-// Warning logs messages of severity WARNING.
-func (l *Logger) Warning(format string, v ...interface{}) {
-	l.log(WARNING, l.basicLogMessage(WARNING, format, v...))
-}
-
-// Info logs messages of severity INFO.
-func (l *Logger) Info(format string, v ...interface{}) {
-	l.log(INFO, l.basicLogMessage(INFO, format, v...))
-}
-
-// Debug logs messages of severity DEBUG.
-func (l *Logger) Debug(format string, v ...interface{}) {
-	l.log(DEBUG, l.basicLogMessage(DEBUG, format, v...))
-}
-
-// Write logs messages of severity WARNING.  This method satisfies the io.Writer
-// interface so that Logger instances can be used as output for Golang's log module.
-func (l *Logger) Write(bytes []byte) (int, error) {
-	l.log(WARNING, l.basicLogMessage(WARNING, strings.TrimSuffix(string(bytes), "\n")))
-	return len(bytes), nil
-}
-
 func (l *Logger) send(s string) {
 	// TODO: Should error handling be added to this? Our io.Writer will likely always be
 	// os.Stdout, what would we do if unable to write to stdout?
 	fmt.Fprintln(l.writer, s)
-}
-
-// This is an (internal) utility method for creating simple LogMessage (functions).
-func (l *Logger) basicLogMessage(level Level, format string, v ...interface{}) func() LogMessage {
-	return func() LogMessage {
-		return LogMessage{
-			Message:   fmt.Sprintf(format, v...),
-			Timestamp: time.Now().Format(time.RFC3339),
-			Service:   ecsService{Name: l.serviceName, Type: l.serviceType},
-			Log:       ecsLog{Level: LevelString(level)},
-		}
-	}
-}
-
-func validLevel(level Level) bool {
-	switch level {
-	case DEBUG, INFO, WARNING, ERROR, FATAL:
-		return true
-	}
-	return false
 }
 
 // LevelString converts log integers to strings
@@ -257,21 +268,10 @@ func LevelString(level Level) string {
 	}
 }
 
-// NewLogger creates a new Logger instance using arguments for an io.Writer, service name and type, and
-// a log level.
-//
-// The serviceName and serviceType arguments correspond to service.name and service.type in the ECS
-// specification (see: https://doc.wikimedia.org/ecs/#ecs-service); Broadly speaking, service type refers
-// to the software system implementing logging, and service name to the instance of that system sending
-// log data.
-//
-// The logLevel argument must be set to one of DEBUG, INFO, WARNING, ERROR, or FATAL.  Only messages
-// logged at this level - or higher - are formatted and output.
-func NewLogger(writer io.Writer, serviceName, serviceType string, logLevel Level) (*Logger, error) {
-
-	if !validLevel(logLevel) {
-		return nil, fmt.Errorf("Unsupported log level: %d", logLevel)
+func validLevel(level Level) bool {
+	switch level {
+	case DEBUG, INFO, WARNING, ERROR, FATAL:
+		return true
 	}
-
-	return &Logger{writer, serviceName, serviceType, logLevel}, nil
+	return false
 }
